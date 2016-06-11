91d9cae0042be957fece6ab336799e43fd7d1c78
dashdots <faculacola@gmail.com>
2016-06-11 20:49:56 +0800

    add example

---
+++ example.js
@@ -0,0 +1,6 @@
+var textNormalize = require('text-normalize');
+
+var str = '！＂＃＄％＆＇（）＊＋，－．／０１２３４５６７８９：；＜＝＞？＠ＡＢＣＤＥＦＧＨＩＪＫＬＭＮＯＰＱＲＳＴＵＶＷＸＹＺ［＼］＾＿｀ａｂｃｄｅｆｇｈｉｊｋｌｍｎｏｐｑｒｓｔｕｖｗｘｙｚ｛｜｝～　。〃〝〞‘’“”';
+
+console.log(textNormalize(str));
+// !"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\]^_`abcdefghijklmnopqrstuvwxyz{|}~ ."""''""
